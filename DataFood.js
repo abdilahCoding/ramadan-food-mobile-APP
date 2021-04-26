f731e7d38fbeb164a023fb4253cacbce62507ca0
abdilahCoding <abdilahelaidii@gmail.com>
2021-04-26 22:27:14 +0000

    app food

---
+++ DataFood.js
@@ -0,0 +1,21 @@
+
+export const DataFood = [
+    {
+        id: '0001',
+        name: "Food1",
+        price: 40,
+        image: require("./assets/img1.jpg")
+    },
+    {
+        id: '0002',
+        name: "Food2",
+        price: 30,
+        image: require("./assets/img2.jpg")
+    },
+    {
+        id: '0003',
+        name: "Food3",
+        price: 20,
+        image: require("./assets/img4.jpg")
+    },
+]
